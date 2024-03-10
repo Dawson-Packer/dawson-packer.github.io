d858604308ce5bda8ca4a4699853e695d0b895d5
Dawson-Packer <Dawson_Packer@outlook.com>
2024-03-09 15:22:13 -0900

    Boot text added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,22 @@
+import { delay, dotdotdot, boot_text } from './boot_sequence.js'
 
 
+async function booting_text() {
 
-function booting_text() {
+    await delay(500);
+    let console_paragraph = document.getElementById("console_paragraph");
+    console_paragraph.innerHTML = "Booting DAWS v0.1";
+    await dotdotdot(console_paragraph, 3);
+    await boot_text(console_paragraph);
 
 
+    
+
+    
+    
 }
 
-function setup() {
+async function setup() {
 
 
 }
@@ -18,6 +28,7 @@ function main() {
 
 
     console.log("hello world!")
+    booting_text();
 
 }
 
